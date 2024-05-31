feddb27162d622b379feb1380a7fdeb51bf2e598
Sweet-Swi <channel.iru0000@gmail.com>
2024-05-31 20:19:23 +0900

    Optimize timeline(WIP)

--- Assets/Scripts/Inspector/Timeline/TimelinePanel.cs
+++ Assets/Scripts/Inspector/Timeline/TimelinePanel.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using ADOFAI;
 using NeoEditor.Tabs;
+using OggVorbisEncoder.Setup;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.UI;
 
 namespace NeoEditor.Inspector.Timeline
@@ -14,23 +16,29 @@ namespace NeoEditor.Inspector.Timeline
     {
         public EffectTabBase tab;
 
-        public GameObject horizontalGrid;
-        public GameObject verticalGrid;
-        public GameObject EventObj;
+        public GameObject horizontalLine;
+        public GameObject verticalLine;
+        public GameObject eventObj;
+        public FloorNumberText floorNum;
 
         public RectTransform root;
-        public GameObject content;
-        public GameObject grid;
-        public GameObject events;
+        public RectTransform content;
+        public RectTransform grid;
+        public RectTransform events;
         public ScrollRect scroll;
+        public RectTransform scrollRT;
+        public RectTransform floorNumBar;
 
         public GameObject playhead;
 
-        private List<GameObject> horizontalLines = new List<GameObject>();
-        private List<GameObject> verticalLines = new List<GameObject>();
-
-        private float scale = 200f;
+        private float width = 200f;
         private float height = 25f;
+        private float scale = 1f;
+        private int vIndex;
+        private int magnetNum = 4;
+        private bool followPlayhead = true;
+        private bool changingScroll = false;
+        private Vector2 prevScrollPos = Vector2.zero;
 
         private LevelEventType[] ignoreEvents = new LevelEventType[]
         {
@@ -69,62 +77,115 @@ namespace NeoEditor.Inspector.Timeline
 
         private TimelineEvent selectedEvent;
 
+        private ObjectPool<GameObject> hPool;
+        private ObjectPool<GameObject> vPool;
+        private ObjectPool<GameObject> eventPool;
+        private ObjectPool<FloorNumberText> floorNumPool;
+
+        private LinkedList<VerticalLineData> vLines = new LinkedList<VerticalLineData>();
+
+        void CreatePool()
+        {
+            hPool = new ObjectPool<GameObject>(
+                () => Instantiate(horizontalLine, grid),
+                (obj) => obj.SetActive(true),
+                (obj) => obj.SetActive(false),
+                (obj) => Destroy(obj),
+                true,
+                20,
+                100
+            );
+            vPool = new ObjectPool<GameObject>(
+                () => Instantiate(verticalLine, grid),
+                (obj) => obj.SetActive(true),
+                (obj) => obj.SetActive(false),
+                (obj) => Destroy(obj),
+                true,
+                20,
+                1000
+            );
+            eventPool = new ObjectPool<GameObject>(
+                () => Instantiate(eventObj, events),
+                (obj) => obj.SetActive(true),
+                (obj) => obj.SetActive(false),
+                (obj) => Destroy(obj),
+                true,
+                20,
+                10000
+            );
+            floorNumPool = new ObjectPool<FloorNumberText>(
+                () => Instantiate(floorNum, floorNumBar),
+                (obj) => obj.gameObject.SetActive(true),
+                (obj) => obj.gameObject.SetActive(false),
+                (obj) => Destroy(obj),
+                true,
+                20,
+                1000
+            );
+
+            scroll.onValueChanged.AddListener(OnValueChanged);
+        }
+
         public void Init()
         {
-            foreach (var gameObject in horizontalLines)
-                Destroy(gameObject);
-            foreach (var gameObject in verticalLines)
-                Destroy(gameObject);
-
-            horizontalLines.Clear();
-            verticalLines.Clear();
-
             NeoEditor editor = NeoEditor.Instance;
+            if (vPool == null)
+                CreatePool();
 
             var floors = editor.floors;
+
+            scroll.content.anchoredPosition = Vector2.zero;
+            float scrollWidth = scrollRT.rect.width;
+            float scrollHeight = scrollRT.rect.height;
+
+            foreach (var line in vLines)
+                vPool.Release(line.obj);
+            vLines.Clear();
 
             foreach (var floor in floors)
             {
                 if (floor.midSpin)
                     continue;
 
-                GameObject line = Instantiate(verticalGrid, grid.transform);
-                line.transform.LocalMoveX(TimeToBeat(floor.entryTime) * scale);
+                float posX = TimeToBeat(floor.entryTime) * width * scale;
+                if (posX < scrollWidth)
+                {
+                    var line = vPool.Get();
+                    var num = floorNumPool.Get();
+                    line.transform.LocalMoveX(posX);
+                    num.transform.LocalMoveX(posX + scroll.content.anchoredPosition.x);
+                    num.text.text = floor.seqID.ToString();
 
-                line.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text =
-                    floor.seqID.ToString();
-                line.SetActive(true);
-
-                verticalLines.Add(line);
+                    line.SetActive(true);
+                    num.gameObject.SetActive(true);
+                    vLines.AddLast(new VerticalLineData(floor.seqID, posX, line, num));
+                }
             }
 
             foreach (var levelEvent in editor.events)
             {
                 if (ignoreEvents.Contains(levelEvent.eventType))
                     continue;
-                GameObject eventObj = Instantiate(EventObj, events.transform);
+                GameObject obj = Instantiate(eventObj, events.transform);
                 scrFloor floor = floors[levelEvent.floor];
 
-                eventObj.transform.GetChild(0).GetComponent<Image>().sprite = GCS.levelEventIcons[
+                obj.transform.GetChild(0).GetComponent<Image>().sprite = GCS.levelEventIcons[
                     levelEvent.eventType
                 ];
-                eventObj.GetComponent<TimelineEvent>().panel = this;
+                obj.GetComponent<TimelineEvent>().panel = this;
 
-                Main.Entry.Logger.Log(levelEvent.ToString());
-
-                Vector2 position = new Vector2(TimeToBeat(floor.entryTime) * scale, -25);
+                Vector2 position = new Vector2(TimeToBeat(floor.entryTime) * width, -25);
                 float bpm = editor.customLevel.levelData.bpm;
                 object f;
                 bool b = levelEvent.data.TryGetValue("angleOffset", out f);
 
-                position += new Vector2((b ? (float)f : 0) / 180f * (1 / floor.speed) * scale, 0);
-                eventObj.transform.LocalMoveX(position.x);
+                position += new Vector2((b ? (float)f : 0) / 180f * (1 / floor.speed) * width, 0);
+                obj.transform.LocalMoveX(position.x);
 
                 b = levelEvent.data.TryGetValue("duration", out f);
-                eventObj
-                    .GetComponent<RectTransform>()
+                obj.GetComponent<RectTransform>()
                     .SizeDeltaX(
-                        b ? Mathf.Max((float)f * scale * (1 / floor.speed), height) : height
+                        b ? Mathf.Max((float)f * width * (1 / floor.speed), height) : height
                     );
             }
 
@@ -132,7 +193,7 @@ namespace NeoEditor.Inspector.Timeline
             content
                 .GetComponent<RectTransform>()
                 .SizeDeltaX(
-                    TimeToBeat(floors[floors.Count - 1].entryTime + conductor.crotchet * 4) * scale
+                    TimeToBeat(floors[floors.Count - 1].entryTime + conductor.crotchet * 4) * width
                 );
             content.GetComponent<RectTransform>().SizeDeltaY(1000f);
         }
@@ -157,12 +218,93 @@ namespace NeoEditor.Inspector.Timeline
             scrConductor conductor = scrConductor.instance;
             if (conductor.song.clip)
             {
-                playhead.transform.LocalMoveX(TimeToBeat(conductor.songposition_minusi) * scale);
-                scroll.content.anchoredPosition = new Vector2(
-                    -playhead.transform.localPosition.x + root.rect.width / 2,
-                    scroll.content.anchoredPosition.y
-                );
+                playhead.transform.LocalMoveX(TimeToBeat(conductor.songposition_minusi) * width);
+
+                if (followPlayhead)
+                {
+                    changingScroll = true;
+                    scroll.content.anchoredPosition = new Vector2(
+                        -playhead.transform.localPosition.x + root.rect.width / 2,
+                        scroll.content.anchoredPosition.y
+                    );
+                    changingScroll = false;
+                }
             }
+        }
+
+        public void OnValueChanged(Vector2 position)
+        {
+            NeoEditor editor = NeoEditor.Instance;
+
+            VerticalLineData CreateLine(scrFloor floor, VerticalLineData prev)
+            {
+                if (floor.midSpin)
+                    return new VerticalLineData(floor.seqID, prev.x, null, null);
+                var line = vPool.Get();
+                var num = floorNumPool.Get();
+                float posX = TimeToBeat(floor.entryTime) * width * scale;
+                line.transform.LocalMoveX(posX);
+                num.transform.LocalMoveX(posX + scroll.content.anchoredPosition.x);
+                num.text.text = floor.seqID.ToString();
+
+                line.SetActive(true);
+                num.gameObject.SetActive(true);
+                return new VerticalLineData(floor.seqID, posX, line, num);
+            }
+
+            Vector2 pos = position * (content.sizeDelta - scrollRT.rect.size);
+            Vector2 dir = prevScrollPos - pos;
+
+            if (dir.x < 0)
+            {
+                var first = vLines.First.Value;
+                while (first.x < pos.x)
+                {
+                    if (first.obj != null)
+                        vPool.Release(first.obj);
+                    if (first.num != null)
+                        floorNumPool.Release(first.num);
+                    vLines.RemoveFirst();
+                    first = vLines.First.Value;
+                }
+                var last = vLines.Last.Value;
+                while (last.x < pos.x + scrollRT.rect.width)
+                {
+                    if (last.id + 1 >= editor.floors.Count)
+                        break;
+
+                    var floor = editor.floors[last.id + 1];
+                    vLines.AddLast(CreateLine(floor, last));
+                    last = vLines.Last.Value;
+                }
+            }
+            else if (dir.x > 0)
+            {
+                var last = vLines.Last.Value;
+                while (last.x > pos.x + scrollRT.rect.width)
+                {
+                    if (last.obj != null)
+                        vPool.Release(last.obj);
+                    if (last.num != null)
+                        floorNumPool.Release(last.num);
+                    vLines.RemoveLast();
+                    last = vLines.Last.Value;
+                }
+                var first = vLines.First.Value;
+                while (first.x > pos.x)
+                {
+                    if (first.id + -1 < 0)
+                        break;
+
+                    var floor = editor.floors[first.id - 1];
+                    vLines.AddFirst(CreateLine(floor, last));
+                    first = vLines.First.Value;
+                }
+            }
+            prevScrollPos = pos;
+
+            //if (!changingScroll && dir.x != 0)
+            //    followPlayhead = false;
         }
 
         float TimeToBeat(double time)
---
+++ Assets/Scripts/Inspector/Timeline/VerticalLineData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NeoEditor.Inspector.Timeline
+{
+    public struct VerticalLineData
+    {
+        public int id;
+        public float x;
+        public GameObject obj;
+        public FloorNumberText num;
+
+        public VerticalLineData(int i, float posX, GameObject line, FloorNumberText floorId)
+        {
+            id = i;
+            x = posX;
+            obj = line;
+            num = floorId;
+        }
+    }
+}
