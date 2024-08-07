90233935ca1ca10f6f01cdf354ee35682c51f67f
Sweet-Swi <channel.iru0000@gmail.com>
2024-08-07 21:20:28 +0900

    feat: popup windows

---
+++ Assets/Scripts/PopupWindows/OkCancelPopup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NeoEditor.PopupWindows
+{
+    public class OkCancelPopup : PopupBase
+    {
+        public Button okButton;
+		public Button cancelButton;
+	}
+}
---
+++ Assets/Scripts/PopupWindows/RenamePopup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NeoEditor.PopupWindows
+{
+    public class RenamePopup : PopupBase
+    {
+        public Button okButton;
+		public Button cancelButton;
+        public TMP_InputField inputField;
+	}
+}
