8506ae884e7663ea6e8c4faf84292dcc7512fda2
Sweet-Swi <channel.iru0000@gmail.com>
2024-09-20 05:21:47 +0900

    fix: mouse cursor

---
+++ Assets/Scripts/Patches/PauseMenuPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NeoEditor.Patches
+{
+    public static class PauseMenuPatch
+	{
+        [HarmonyPatch(typeof(PauseMenu), "UpdateCursorVisibility")]
+        public static class CursorVisibilityPatch
+        {
+            public static void Postfix()
+            {
+                if (NeoEditor.Instance == null)
+                    return;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
